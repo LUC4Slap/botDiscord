aa60c01c6a3d8447bafd064ae596ada0f427bf53
lucas__almeida <lucaslap27@gmail.com>
2022-10-03 14:37:41 -0300

    Ajustes

--- bot.js
+++ bot.js
@@ -17,11 +17,25 @@ client.on("messageCreate", (message) => {
   // console.log(JSON.stringify(message, null, 2));
   const args = message.content.trim().slice(config.prefix.length).split(/!+/g);
   // console.log(args);
-  const command = args[1].toLowerCase();
+  const command = args[0].split(" ")[0].toLowerCase();
   try {
     const commandFile = require(`./commands/${command}.js`);
     commandFile.run(client, message, args);
   } catch (error) {
     console.log(`Error: ${error}`);
+  }
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const { commandName } = interaction;
+
+  if (commandName === "ping") {
+    await interaction.reply("Pong!");
+  } else if (commandName === "server") {
+    await interaction.reply("Server info.");
+  } else if (commandName === "user") {
+    await interaction.reply("User info.");
   }
 });
--- commands/musica.js
+++ commands/musica.js
@@ -4,7 +4,7 @@ const {
   createAudioPlayer,
   createAudioResource,
 } = require("@discordjs/voice");
-const { EmbedBuilder } = require("discord.js");
+const { EmbedBuilder, ChannelType, MessageManager } = require("discord.js");
 const ytdl = require("ytdl-core");
 const config = require("../config.json");
 
@@ -12,50 +12,51 @@ module.exports = {
   name: "musica",
   author: "Lucas",
   run: async (client, message, args) => {
-    const streamOptions = {
-      seek: 0,
-      volume: 1,
-    };
-
-    let channel = null;
-    try {
-      channel = await message.guild.channels.fetch(config.channel_id);
-    } catch (error) {
-      let cor_da_embed = 0x0099ff;
-      let embed_1 = new EmbedBuilder()
-        .setColor(cor_da_embed)
-        .setDescription(`Canal não encontrado`);
-      await message
-        .reply({ content: `${message.author}`, embeds: [embed_1] })
-        .then((msg) => {
-          setTimeout(() => {
-            msg.edit({ embeds: [embed_2] });
-          }, 2000);
-        });
-      return;
-    }
-
-    if (channel != null) {
-      console.log("Canal encontrado de client");
-      const connection = await joinVoiceChannel({
-        channelId: channel.id,
-        guildId: channel.guild.id,
-        adapterCreator: channel.guild.voiceAdapterCreator,
-      });
-      if (connection) {
-        const strem = ytdl(args[2].trim(), { filter: "audioonly" });
-        const player = createAudioPlayer();
-        const resource = createAudioResource(strem);
-
-        await player.play(resource);
-        // const DJ = connection.playStream(strem, streamOptions);
-        connection.subscribe(player);
-        player.on("error", (error) => console.error(error));
-        player.on(AudioPlayerStatus.Idle, () => {
-          console.log(`song's finished`);
-          connection.disconnect();
-        });
-      }
-    }
+    // let authorID = JSON.parse(message);
+    // let link = args[0].split(" ")[1];
+    // const streamOptions = {
+    //   seek: 0,
+    //   volume: 1,
+    // };
+    // let channel = null;
+    // try {
+    //   channel = await message.guild.channels.fetch(config.channel_id);
+    // } catch (error) {
+    //   let cor_da_embed = 0x0099ff;
+    //   let embed_1 = new EmbedBuilder()
+    //     .setColor(cor_da_embed)
+    //     .setDescription(`Canal não encontrado`);
+    //   await message.reply({ content: `${message.author}`, embeds: [embed_1] });
+    //   return;
+    // }
+    // if (channel.type != ChannelType.GuildVoice) {
+    //   let cor_da_embed = 0x0099ff;
+    //   let embed_1 = new EmbedBuilder()
+    //     .setColor(cor_da_embed)
+    //     .setDescription(`Canal selecionado não é de voz`);
+    //   await message.reply({ content: `${message.author}`, embeds: [embed_1] });
+    //   return;
+    // }
+    // if (channel != null) {
+    //   console.log("Canal encontrado de client");
+    //   const connection = await joinVoiceChannel({
+    //     channelId: channel.id,
+    //     guildId: channel.guild.id,
+    //     adapterCreator: channel.guild.voiceAdapterCreator,
+    //   });
+    //   if (connection) {
+    //     const strem = ytdl(link.trim(), { filter: "audioonly" });
+    //     const player = createAudioPlayer();
+    //     const resource = createAudioResource(strem);
+    //     await player.play(resource);
+    //     // const DJ = connection.playStream(strem, streamOptions);
+    //     connection.subscribe(player);
+    //     player.on("error", (error) => console.error(error));
+    //     player.on(AudioPlayerStatus.Idle, () => {
+    //       console.log(`song's finished`);
+    //       connection.disconnect();
+    //     });
+    //   }
+    // }
   },
 };
